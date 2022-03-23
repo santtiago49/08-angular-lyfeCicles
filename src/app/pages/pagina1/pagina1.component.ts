6b408de072eba57906b513bc2d47c94b860580c8
Santiago <santtiago49@gmail.com>
2022-03-23 12:25:21 -0300

    prueba de timerSubscription

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -1,4 +1,5 @@
 import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pagina1',
@@ -11,6 +12,8 @@ export class Pagina1Component implements
        AfterViewChecked, OnDestroy {
 
   nombre: string = "santiago";
+  segundos: number = 0;
+  timerSubscription!: Subscription;
 
   constructor() { 
     console.log('constructor');
@@ -29,13 +32,16 @@ export class Pagina1Component implements
     console.log('ngAfterViewChecked');
   }
   ngOnDestroy(): void {
-    console.log('ngOnDestroy');
+    this.timerSubscription.unsubscribe();
+    console.log('timer limpiado');
   }
   ngDoCheck(): void {
     console.log('ngDoCheck');
   }
+
   ngOnInit(): void {
     console.log('ngOnInit');
+    this.timerSubscription = interval(1000).subscribe( i => this.segundos = i )
   }
   ngOnChanges(): void {
     console.log('ngOnChanges');
